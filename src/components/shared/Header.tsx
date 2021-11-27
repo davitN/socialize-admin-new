2dde65fce743c6d477fb8ff6574f938ea6e4a807
musera <irakli.museridze@ol.ge>
2021-11-27 17:33:03 +0400

    log out

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
+import { useDispatch } from 'react-redux';
+import { logoutAction } from '../../store/ducks/authDuck';
 const useStyles = createUseStyles({
   container: {
     height: '70px',
     backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  logOut: {
+    alignSelf: 'flex-end',
+    marginRight: 20,
+    fontSize: 18,
   },
 });
 const Header: React.FC<{}> = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    console.log('asdfasdf');
+    dispatch(logoutAction());
+  };
+
   return (
-    <div className={`flex-horizontal ${classes.container}`}>
+    <div className={`flex-vertical ${classes.container}`}>
+      <label className={classes.logOut} onClick={logOut}>log out</label>
       {/*<Link to="/companies">companies</Link> | <Link to="/app-users">app users</Link> | <Link to="/auth">log in</Link> |{" "}*/}
       {/*<Link to="/dashboard">dashboard</Link>*/}
     </div>
