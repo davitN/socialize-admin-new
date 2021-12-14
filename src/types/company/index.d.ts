33ccd89e4f3b100f8207165c921955ac9bc000e1
Erekle <ghonghadze.erekle@gmail.com>
2021-12-14 22:06:48 +0400

    table in companies

--- src/types/company/index.d.ts
+++ src/types/company/index.d.ts
@@ -1,12 +1,23 @@
 export interface CompanyModel {
-  _id?: string,
-  placeId?: string,
-  adminId?: string,
-  name?: string,
-  companySubscription?: string,
-  paidTill?: string,
-  createdAt?: string,
-  updatedAt?: string,
-  isActive?: boolean,
-  __v?: number
+  _id?: string;
+  placeId?: string;
+  adminId?: string;
+  name?: string;
+  companySubscription?: {
+    description?: string;
+    name?: string;
+    ordering?: number;
+    __v?: number;
+    _id?: string;
+  };
+  paidTill?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  isActive?: boolean;
+  __v?: number;
+}
+
+export interface CompanyTableModel {
+  count: number;
+  data: Array<CompanyModel>;
 }
