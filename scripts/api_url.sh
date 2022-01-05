4a791e3dffe5c20aaf93536d40dc15abb1cb42b5
George Qoridze <george@qoridze.com>
2022-01-05 14:11:00 +0400

    admin - dev env, elif for backend url proofing

--- scripts/api_url.sh
+++ scripts/api_url.sh
@@ -3,7 +3,7 @@
 if [ "$BUILD_ENV" = "prod"  ]; then
   echo "Production build, replacing API URL..."
   sed -Ei "s/(export const backendUrl =).*/\1 'https\:\/\/aws.thatsocialapp.com\/admin\/';/g"  src/services/credentials.service.ts
-elif [ "$BUILD_ENV" = "prod"  ]; then
+elif [ "$BUILD_ENV" = "dev"  ]; then
   echo "Developemnt build, replacing API URL..."
   sed -Ei "s/(export const backendUrl =).*/\1 'https\:\/\/aws-dev.thatsocialapp.com\/admin\/';/g"  src/services/credentials.service.ts
 else
